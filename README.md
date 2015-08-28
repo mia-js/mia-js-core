b99b6be8a1aab2c13dba441bf39e495d9990f56b
Kamil Fayzullin <ka.fayzullin@gmail.com>
2015-08-28 18:59:58 +0200

    Initial public commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mia-js-core
-mia.js - Middleware API framework - Core module 
+mia.js - Middleware API framework - Core module
+See docs of mia.js project.
---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports.Run = require('./lib/mia-js');
+module.exports.Shared = require('./node_modules/shared');
+module.exports.Utils = require('./node_modules/utils');
+module.exports.ErrorHandler = require('./node_modules/errorHandler');
+module.exports.BaseClass = require('./node_modules/baseClass');
+module.exports.BaseModel = require('./node_modules/baseModel');
+module.exports.RoutesHandler = require('./node_modules/routesHandler');
+module.exports.DbAdapters = require('./node_modules/dbAdapters');
+module.exports.ModuleLoader = require('./node_modules/moduleLoader');
+module.exports.CronJobs = require('./node_modules/cronJobs');
+module.exports.Logger = require('./node_modules/logger');
+module.exports.Request = require('./node_modules/request');
+module.exports.GetTranslations = require('./node_modules/translator');
+module.exports.Cached = require('./node_modules/cacheHandler');
+module.exports.ModelValidator = require('./node_modules/modelValidator');
+
+
